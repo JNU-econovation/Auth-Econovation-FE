3c3cf1a3dad5a9112c5d0ffb56c483a9f4d5e6a5
박건규 <geongyu09@gmail.com>
2026-01-31 17:21:55 +0900

    feat: App 컴포넌트 리팩토링 및 스타일 시트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ScreenWrapper from "@shared/layout/ScreenWrapper";
+import Spacing from "@shared/layout/Spacing";
+import TextFieldLayout from "@shared/layout/TextFieldLayout";
+import DefaultButton from "@shared/ui/DefaultButton";
+import Text from "@shared/ui/Text";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      {/* Tailwind CSS 테스트 */}
-      <div className="bg-blue-500 text-white p-4 rounded-lg mt-4">
-        Tailwind is working! 🎉
-      </div>
-
-      {/* Pretendard 폰트 굵기 테스트 */}
-      <div className="space-y-2 mt-4">
-        <p className="font-thin">Pretendard Thin (100) - 한글 테스트</p>
-        <p className="font-light">Pretendard Light (300) - 한글 테스트</p>
-        <p className="font-normal">Pretendard Regular (400) - 한글 테스트</p>
-        <p className="font-medium">Pretendard Medium (500) - 한글 테스트</p>
-        <p className="font-semibold">Pretendard SemiBold (600) - 한글 테스트</p>
-        <p className="font-bold">Pretendard Bold (700) - 한글 테스트</p>
-        <p className="font-extrabold">Pretendard ExtraBold (800) - 한글 테스트</p>
-        <p className="font-black">Pretendard Black (900) - 한글 테스트</p>
-      </div>
-    </>
-  )
+    <ScreenWrapper>
+      <Spacing size={48} direction="vertical" />
+      <Text size="1">Econovation</Text>
+      <Spacing size={28} direction="vertical" />
+      <Text size="4">하나의 아이디로 동아리 서비스들을 이용해보세요!</Text>
+      <Spacing size={50} direction="vertical" />
+      <TextFieldLayout label="id" placeholder="아이디를 입력해주세요." />
+      <Spacing size={24} direction="vertical" />
+      <TextFieldLayout
+        label="password"
+        placeholder="비밀번호를 입력해주세요."
+        type="password"
+        helperTextColor="info"
+      />
+      <Spacing size={24} direction="vertical" />
+      <DefaultButton title="로그인 하기" fullWidth />
+    </ScreenWrapper>
+  );
 }
 
-export default App
+export default App;
