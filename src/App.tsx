cb8c5e38e26ded047985b9a1f695452734fbdc70
박건규 <geongyu09@gmail.com>
2026-01-31 23:44:15 +0900

    feat: App 컴포넌트에서 로그인 및 회원가입 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import ScreenWrapper from "@shared/layout/ScreenWrapper";
-import Spacing from "@shared/layout/Spacing";
-import TextFieldLayout from "@shared/layout/TextFieldLayout";
-import DefaultButton from "@shared/ui/DefaultButton";
-import Text from "@shared/ui/Text";
+import { Routes, Route } from "react-router";
+import LoginPage from "@pages/LoginPage";
+import SignUpPage from "@pages/SignUpPage";
 
 function App() {
   return (
-    <ScreenWrapper>
-      <Spacing size={48} direction="vertical" />
-      <Text size="1">Econovation</Text>
-      <Spacing size={28} direction="vertical" />
-      <Text size="4">하나의 아이디로 동아리 서비스들을 이용해보세요!</Text>
-      <Spacing size={50} direction="vertical" />
-      <TextFieldLayout label="id" placeholder="아이디를 입력해주세요." />
-      <Spacing size={24} direction="vertical" />
-      <TextFieldLayout
-        label="password"
-        placeholder="비밀번호를 입력해주세요."
-        type="password"
-        helperTextColor="info"
-      />
-      <Spacing size={24} direction="vertical" />
-      <DefaultButton title="로그인 하기" fullWidth />
-    </ScreenWrapper>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/sign-in" element={<SignUpPage />} />
+    </Routes>
   );
 }
 
