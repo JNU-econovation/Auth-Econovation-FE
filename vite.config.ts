fdd7876de54a7433397568f7c284c667107460ff
박건규 <geongyu09@gmail.com>
2026-02-01 13:35:00 +0900

    feat: tsconfig 및 vite.config에 새로운 경로 별칭 추가
    #7

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@shared': path.resolve(__dirname, './src/components/common/shared'),
-      '@entities': path.resolve(__dirname, './src/components/common/entities'),
-      '@pages': path.resolve(__dirname, './src/pages'),
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@shared": path.resolve(__dirname, "./src/components/common/shared"),
+      "@entities": path.resolve(__dirname, "./src/components/common/entities"),
+      "@pages": path.resolve(__dirname, "./src/components/feature/pages"),
+      "@widget": path.resolve(__dirname, "./src/components/feature/widget"),
+      "@app": path.resolve(__dirname, "./src/app"),
     },
   },
-})
+});
