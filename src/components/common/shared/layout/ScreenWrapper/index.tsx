603f21c53ecc9d18c4d897dc2bda500e62e21c2e
박건규 <geongyu09@gmail.com>
2026-02-01 13:35:41 +0900

    feat: ScreenWrapper 컴포넌트에 설명 주석 추가
    #7

--- src/components/common/shared/layout/ScreenWrapper/index.tsx
+++ src/components/common/shared/layout/ScreenWrapper/index.tsx
@@ -1,5 +1,12 @@
 import { PropsWithChildren } from "react";
 
+/**
+ *
+ * 이 컴포넌트는 모든 페이지의 width 크기를 제한하는 공통 컨포넌트입니다.
+ *
+ * 페이지를 나타내는 컴포넌트의 최상단에만 사용되어야합니다.
+ */
+
 const ScreenWrapper = ({ children }: PropsWithChildren) => {
   return <div className="max-w-3xl mx-auto sm:px-0 px-4">{children}</div>;
 };
